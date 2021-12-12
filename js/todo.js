c0a983e6367f64f3cb0be28b7c17f5842828dfdc
fubu1997 <fubu1997@naver.com>
2021-12-12 23:08:35 +0900

    Chrome = Make ToDo part3-1

--- js/todo.js
+++ js/todo.js
@@ -4,15 +4,18 @@ const toDoList = document.getElementById('todo-list');
 
 const toDos = [];
 
+// ToDo를 Localstorage에 저장하는 함수
 function saveToDos() {
   localStorage.setItem("todos", JSON.stringify(toDos)); //JSON.stringify 는 모든요소들을 문자열로 변환
 }
 
+// ToDo를 삭제하는 함수
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
+// ToDo를 HTML요소에 보여주는 함수
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
@@ -25,6 +28,7 @@ function paintToDo(newTodo) {
   toDoList.appendChild(li);
 }
 
+// ToDo를 입력하는 함수
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
@@ -34,4 +38,5 @@ function handleToDoSubmit(event) {
   saveToDos();
 }
 
+//ToDo 입력폼에 입력('submit')해서 함수호출
 toDoForm.addEventListener('submit', handleToDoSubmit);
